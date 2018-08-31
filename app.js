c208ce6f0450cb7fc6c17751770491bc5cf00047
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-31 18:40:04 +0400

    Add MongoDB support

--- app.js
+++ app.js
@@ -4,14 +4,26 @@ const Commando = require('discord.js-commando');
 const path = require('path');
 const sqlite = require('sqlite');
 const config = require('./config.json');
+const MongoClient = require('mongodb').MongoClient;
 const client = new Commando.Client({
     owner: process.env.BOT_OWNER || config.owner,
     commandPrefix: config.prefix || ';',
     disableEveryone: true,
     unknownCommandResponse: false
 });
+
+
+MongoClient.connect(process.env.DATABASE_URL, { useNewUrlParser: true }).then(db => {
+    client.db = db;
+        client.database = db.db(process.env.DATABASE_NAME);
+}).catch(error => {
+    throw `Failed connecting to database\n${error}`;
+});
+
+
 require('./events/event.js')(client);
 global.BOT_DEFAULT_COLOR = config.defaultColor || [233, 91, 169];
+
 
 client.setProvider(
     sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => {
