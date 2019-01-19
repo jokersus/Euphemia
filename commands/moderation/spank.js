c953df88acbe8f25a078d64a20b815d2e7393b2a
jokersus <33132401+jokersus@users.noreply.github.com>
2019-01-19 16:12:29 +0400

    Fix stuff [3]

--- commands/moderation/spank.js
+++ commands/moderation/spank.js
@@ -24,7 +24,7 @@ module.exports = class extends Command {
 					const flag = Math.random() * 100 < 15;
 					setUnmuteTimeout(this.client, message.member);
 					let messageBody = `${targetMember.toString()} has been spanked by ${message.member.toString()}`;
-					if (flag  < 20) {	// 20% chance
+					if (flag) {	// 20% chance
 						messageBody += ' ' + SPANK_EMOTE;
 					}
 					return message.channel.send(messageBody);
