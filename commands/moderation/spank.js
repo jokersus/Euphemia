3b09ac47db6ca03ce7fd7737a5e099175435c9ac
jokersus <33132401+jokersus@users.noreply.github.com>
2019-01-19 15:52:32 +0400

    Added spank command (untested)

---
+++ commands/moderation/spank.js
@@ -0,0 +1,40 @@
+const { Command } 	= require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+const SPANK_ROLE_ID = '422621940868579338'				// Geassed members
+const SPANK_EMOTE 	= '<a:LULuche:487228139206868993>'
+
+module.exports = class extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'spank',
+			group: 'moderation',
+			memberName: 'spank',
+			description: 'Spanks naughty people',
+			clientPermissions: ['MANAGE_ROLES'],
+			guildOnly: true
+		});
+	}
+
+   async run(message) {
+	   if (message.member.hasPermission('MANAGE_ROLES')) {
+		   if (!message.mentions.members.size) {
+			   if (messqage.member.hasPermission('MANAGE_ROLES')) {
+					const targets = message.mentions.members;
+					await targets.tap(async member => member.addRole(SPANK_ROLE_ID));
+					const flag = Math.random() * 100 < 15;
+					return message.send(`${targets
+						.map(member => member.toString())
+						.join(' ')}
+						${targets.size === 1 ? '' : ' all '} got spanked by ${message.member.toString()}
+						${flag ? SPANK_EMOTE : ''}
+					`);
+			   }
+		   } else {
+			   return message.replt('Are you trying to spank thin air?')
+		   }
+	   } else {
+		   await message.member.addRole(SPANK_ROLE_ID);
+		   return message.reply('Nice try. You got yourself spanked.');
+	   }
+	}
+};
