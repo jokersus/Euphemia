5fce9b3e5bdf232fde5e3bcc1b9dc9a65c6a4260
jokersus <lolivampireslave@gmail.com>
2021-07-02 01:26:50 +0400

    Slash command integrating testing code

--- events/bot/ready.js
+++ events/bot/ready.js
@@ -1,3 +1,50 @@
 module.exports = async client => {
 	console.log(`Logged in as ${client.user.tag}!`);
+
+	// const command = await client.guilds.resolve('344556222755766283').commands.create({
+	// 	name: 'ping',
+	// 	description: 'Pings you idfk',
+	// 	options: [
+	// 		{
+	// 			type: 'STRING',
+	// 			name: 'option1',
+	// 			description: 'ok1',
+	// 			required: true,
+	// 		},
+	// 		{
+	// 			type: 'USER',
+	// 			name: 'someuser',
+	// 			description: 'You gotta select a user ok',
+	// 			required: true,
+	// 			// choices: [
+	// 			// 	{
+	// 			// 		name: 'Number of whatever',
+	// 			// 		value: 123
+	// 			// 	},
+	// 			// 	{
+	// 			// 		name: 'Second of whatever',
+	// 			// 		value: 234
+	// 			// 	}
+	// 			// ]
+	// 		},
+	// 		{
+	// 			type: 'BOOLEAN',
+	// 			name: 'option3',
+	// 			description: 'ok3',
+	// 			required: false
+	// 		}
+	// 	]
+	// });
+	//
+	// console.log(command);
+
+	// client.on('interaction', async interaction => {
+	// 	if (!interaction.isCommand()) {
+	// 		return;
+	// 	}
+	//
+	// 	if (interaction.commandName === 'ping') {
+	// 		await interaction.reply('Pong');
+	// 	}
+	// });
 };
